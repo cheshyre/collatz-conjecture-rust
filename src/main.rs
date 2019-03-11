4e541bb89bfb8130e1800d92b396875b14f6286b
Matthias Heinz <cheshyre@matthiass-mbp.speedport_w_724v_typ_a_05011603_06_001>
2019-03-11 22:16:58 +0100

    Premature optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::io;
+// use std::io;
 
 fn do_step(x: u32) -> u32 {
     if x % 2 == 0 {
@@ -10,7 +10,7 @@ fn do_step(x: u32) -> u32 {
 
 fn main() {
     let bottom: u32 = 10000;
-    let top: u32 = 100000;
+    let top: u32 = 10000000;
     // let bottom: u32 = 4;
     // let top: u32 = 10;
 
@@ -21,21 +21,20 @@ fn main() {
     cache.insert(1, 0);
 
     for val in bottom..top {
-        let mut stack = vec![];
         let mut cur_val = val;
+        let mut added_dist = 0;
         while !cache.contains_key(&cur_val) {
-            stack.push(cur_val);
             cur_val = do_step(cur_val);
+            added_dist += 1;
         }
 
         let mut dist = *cache.get(&cur_val).unwrap();
-        while stack.len() != 0 {
-            dist += 1;
-            cur_val = stack.pop().unwrap();
-            cache.insert(cur_val, dist);
+        if added_dist > 0 {
+            cache.insert(val, dist + added_dist);
         }
 
-        // println!("{} {}", val, dist);
+        dist += added_dist;
+
         if dist > max_steps {
             max = val;
             max_steps = dist;
